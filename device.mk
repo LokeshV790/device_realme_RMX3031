bae9aa320940b985e6a6373d2f33e7cb70b9798c
kubersharma001 <kubersharma001@gmail.com>
2022-05-18 06:39:02 +0530

    RMX3031: Don't inherit updatable_apex.mk This is because of the following reasons: - Many build scripts force disable it anyways by settings OVERRIDE_TARGET_FLATTEN_APEX env flag so why bother rendering differences in official and local builds [1][2]. - Compressed apex causes issues with OTA on A only devices [3]. - Compressed apex may cause issues on devices which do not meet kernel requirements [4]. - Highly anticipated mainline modules update aka Play System updates is limited to OEMs/Pixels only [5], we don't do such things on AOSP; shipping Pixel prebuilt apex components feels somewhat unethical.
    Ref:
    [1]: https://github.com/lineageos-infra/build-config/blob/8e435ce7ff7c74097b3500e143380273e099aefb/android/build.sh#L9
    [2]: https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/updatable_apex.mk#L19
    [3]: RecoverySystemService: Failed to reserve space for compressed apex
    [4]: https://source.android.com/devices/tech/ota/apex#kernel-requirements
    [5]: "pushed to end-user devices, either by Google (using the Google Play Store infrastructure) or by the Android partner (using a partner-provided OTA mechanism)." as stated in https://source.android.com/devices/architecture/modular-system#architecture
    
    Also, Remove ro.apex.updatable prop from vendor_prop.mk.
    
    test: m, boots and ota.
    
    Change-Id: I5b5c2366398791100fa2222ef779ca4d90842211
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- device.mk
+++ device.mk
@@ -19,12 +19,6 @@ DEVICE_PATH := device/realme/RMX3031
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Parts
-$(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
-
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Call proprietary blob setup
