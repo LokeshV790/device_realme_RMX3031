8639725c6281fc8092a845e8a5554bed782e2c73
Sarthak Roy <sarthakroy2002@gmail.com>
2022-06-25 13:15:42 +0530

    RMX3031: overlay: Add settings provider
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: LokeshV790 <lokeshvazirani29@gmail.com>

--- device.mk
+++ device.mk
@@ -111,7 +111,8 @@ PRODUCT_PACKAGES += \
     SystemUIOverlay \
     SettingsOverlay \
     TelephonyOverlay \
-    CarrierConfigOverlay
+    CarrierConfigOverlay \
+    SettingsProviderOverlayRMX3031
 
 # Enforce RRO targets
 PRODUCT_ENFORCE_RRO_TARGETS := *
